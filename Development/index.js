a6c34d3df35e34e0e13eba6a2857af043c43a9cc
Caleb Vincent <vincent_806@yahoo.com>
2020-01-20 20:34:13 -0600

    Finished development

---
+++ Development/index.js
@@ -0,0 +1,77 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const axios = require('axios').default;
+const generateHTML = require('./generateHTML');
+const pdf = require('html-pdf');
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "What is your GitHub Username?",
+            name: "username"
+        },
+        {
+            type: "list",
+            message: "What is your favorite color?",
+            choices: [
+                "green",
+                "blue",
+                "pink",
+                "red"
+            ],
+            name: "color"
+        },
+    ])
+    .then(function (userInfo) {
+
+        const queryUrl = `https://api.github.com/users/${userInfo.username}`;
+        const queryStarUrl = `https://api.github.com/users/${userInfo.username}/starred`;
+
+        githubQuery(queryUrl).then(function (data) {
+            githubQueryStars(queryStarUrl).then(function (responseStars) {
+
+                var options = { format: 'Letter' };
+                pdf.create(generateHTML(userInfo, responseStars, data), options).toFile(`./${userInfo.username}.pdf`, function (err, res) {
+                    if (err) return console.log(err);
+                    console.log(res);
+                });
+            })
+        })
+
+
+    });
+
+
+function githubQuery(queryUrl) {
+    return axios.get(queryUrl)
+        .then(function (response) {
+
+            let data = {
+                fullName: (response.data.name),
+                profileImg: (response.data.avatar_url + ".png"),
+                gitHubUsername: (response.data.login),
+                userCity: (response.data.location),
+                userGitHubProfile: (response.data.html_url),
+                userBlog: (response.data.blog),
+                userBio: (response.data.bio),
+                userRepos: (response.data.public_repos),
+                userFollowers: (response.data.followers),
+                userFollowing: (response.data.following)
+            };
+
+
+
+
+            return data;
+        });
+};
+
+function githubQueryStars(queryStarUrl) {
+
+    return axios.get(queryStarUrl)
+        .then(function (responseStars) {
+
+            return responseStars.data.length;
+        });
+};
